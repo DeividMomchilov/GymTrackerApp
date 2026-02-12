41249ba05473e176bd3880e1e0e82b36ed82e74d
Deivid Momchilov <deividmom2006@gmail.com>
2026-02-12 18:41:57 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymTrackerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
